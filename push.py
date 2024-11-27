e25ef42bf9e21b439db13a1f925ae8387eab8822
Jeewhan <distinct19@gmail.com>
2024-11-28 01:04:08 +0900

    feat: update check

--- push.py
+++ push.py
@@ -7,12 +7,33 @@ class Channel(str, Enum):
     책읽어또 = "책읽어또"
 
 
+class Checker:
+    def check(self):
+        raise NotImplementedError
+
+
+class 다진마늘(Checker):
+    def check(self):
+        pass
+
+
+class 책읽어또(Checker):
+    def check(self):
+        pass
+
+
 cli = Typer()
 
 
 @cli.command()
 def check(channel: Channel):
-    print(channel)
+    match channel:
+        case Channel.다진마늘:
+            checker = 다진마늘()
+        case Channel.책읽어또:
+            checker = 책읽어또()
+
+    checker.check()
 
 
 if __name__ == "__main__":
