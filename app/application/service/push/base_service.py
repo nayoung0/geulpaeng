d0f4aec17efce2471a2a4941da7c00afcedd0982
Jeewhan <distinct19@gmail.com>
2024-11-29 05:35:58 +0900

    feat: base_service

--- app/application/service/push/base_service.py
+++ app/application/service/push/base_service.py
@@ -12,15 +12,15 @@ load_dotenv()
 gc = gspread.service_account_from_dict(
     {
         "type": "service_account",
-        "project_id": os.getenv("project_id"),
-        "private_key_id": os.getenv("private_key_id"),
-        "private_key": os.getenv("private_key").replace("\\n", "\n"),
-        "client_email": os.getenv("client_email"),
-        "client_id": os.getenv("client_id"),
+        "project_id": os.getenv("PROJECT_ID"),
+        "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+        "private_key": os.getenv("PRIVATE_KEY").replace("\\n", "\n"),
+        "client_email": os.getenv("CLIENT_EMAIL"),
+        "client_id": os.getenv("CLIENT_ID"),
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": os.getenv("client_x509_cert_url"),
+        "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
         "universe_domain": "googleapis.com",
     }
 )
